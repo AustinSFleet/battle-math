b2be9de8599a375553dca5e2265f6b0f449e7ff5
Mark Strelecky <streleck@gmail.com>
2017-10-16 16:11:23 -0500

    file system

--- client/src/Login&CreateNew/Login.js
+++ client/src/Login&CreateNew/Login.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 
 class Login extends Component {
 
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
-      let inline = {display : "inline-block" };
+
     return (
 
     <div>
       <form>
         <h3>Name</h3>
-        <input type="text"/>
+        <input
+          type="text"
+          onChange = {this.handleInputChange}
+        />
         <h3>Password</h3>
         <input type="password"/>
       </form>
