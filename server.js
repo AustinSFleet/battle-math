c555861f2c3e8b3fefb50313c39fb2d22435fbac
tathagata218 <tathagata218@yahoo.com>
2017-10-22 18:14:57 -0500

    this is the suff

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ db.user_Info.findAll({
 
 app.put("/api/save",(req,res) => {
   console.log(req.body);
-db.user_Info.update({experience : req.body.experience, level : req.body.level, coins : req.body.coin ,HP : req.body.HP},{ where : {userName :req.body.name}}).then(
+db.user_Info.update({experience : req.body.experience, level : req.body.level, coins : req.body.coins ,HP : req.body.HP},{ where : {userName :req.body.name}}).then(
   function(){
     res.json(true);
     console.log('it worked!!!');
