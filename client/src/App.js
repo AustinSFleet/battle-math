b6a0c46384445c694f9b4971c555bd8900a48d3b
Mark Strelecky <streleck@gmail.com>
2017-10-22 15:26:16 -0500

    animations

--- client/src/App.js
+++ client/src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     me: {
       name: "Redman",
       level: 5,
-      experience: 0,
+      experience: 200,
       abilities: [],
       items: [],
       maxHP: 20,
-      img:"/images/Finn.png",
+      img:"/images/batman.png",
       HP: 12,
       maxHP: 12,
       coins:0
@@ -36,6 +36,7 @@ class App extends Component {
       me: upMe
     })
   }
+
 
   handleInputChange = event => {
     const { name, value } = event.target;
