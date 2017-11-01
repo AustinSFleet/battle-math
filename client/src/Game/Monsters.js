430fdc17fa02860104dbef1460b1abd0b400c5a3
AustinSFleet <austinsfleet@gmail.com>
2017-11-01 12:49:27 -0500

    changed monster HP

--- client/src/Game/Monsters.js
+++ client/src/Game/Monsters.js
@@ -3,9 +3,9 @@ export default {
 
 monsters: [{
   name: "Trump",
-  experience: 30,
+  experience: 50,
   attack: ()=> {
-    return(300 + Math.floor(2 * Math.random()))},
+    return(30 + Math.floor(2 * Math.random()))},
   img:"/images/trump5battle.png",
   HP: 10,
   maxHP: 10,
@@ -13,7 +13,7 @@ monsters: [{
 },
 {
   name: "Teenager",
-  experience: 30,
+  experience: 50,
   attack: ()=> {
     return(2 + Math.floor(2 * Math.random()))},
   img:"/images/Tiffany.png",
@@ -23,11 +23,11 @@ monsters: [{
 },
 {
   name: "Marshall",
-  experience: 30,
+  experience: 20,
   attack: ()=> {
     return(2 + Math.floor(2 * Math.random()))},
   img:"/images/Marshall_Lee.png",
-  HP: 4,
+  HP: 20,
   maxHP: 12,
   monCoins: 30
 },
@@ -37,7 +37,7 @@ monsters: [{
   attack: ()=> {
     return(1 + Math.floor(1 * Math.random()))},
   img:"/images/Gnome_ruler.png",
-  HP: 40,
+  HP: 20,
   maxHP: 30,
   monCoins:20
 },
@@ -47,7 +47,7 @@ monsters: [{
   attack: ()=> {
     return(1 + Math.floor(1 * Math.random()))},
   img:"/images/greg.png",
-  HP: 4,
+  HP: 12,
   maxHP: 30,
   monCoins:50
 },
@@ -67,7 +67,7 @@ monsters: [{
   attack: ()=> {
     return(5 + Math.floor(1 * Math.random()))},
   img:"/images/bobaBattle.png",
-  HP: 4,
+  HP: 10,
   maxHP: 40,
   monCoins:70
 }
